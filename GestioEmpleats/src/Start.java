ce22c5e5abc76e9f3dd4fd10a835163e061c93f4
Datzu <Degor@Degor-Portatil>
2013-11-13 17:20:09 +0100

    Added guidelines.txt and removed useless code

--- GestioEmpleats/src/Start.java
+++ GestioEmpleats/src/Start.java
@@ -2,13 +2,7 @@
 public class Start {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Start");
-		System.out.println("Start2");
-		System.out.println("Start3");
-		System.out.println("Start4");
-		System.out.println("Start6");
-		System.out.println("Start7");
+		
 	}
-
+	
 }
