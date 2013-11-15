bf41257d3fe11e55697cc318dca8e9f32d1a8674
Gerard <Gerard@Gerard-Acer>
2013-11-15 20:53:31 +0100

    first frame

--- GestioEmpleats/src/Start.java
+++ GestioEmpleats/src/Start.java
@@ -1,5 +1,14 @@
+import javax.swing.JFrame;
+import com.gestioempleats.components.FirstFrame;
+import java.awt.BorderLayout;
 
-public class Start {
+
+public class Start extends JFrame {
+	public Start() {
+		
+		FirstFrame firstFrame = new FirstFrame();
+		getContentPane().add(firstFrame, BorderLayout.NORTH);
+	}
 
 	public static void main(String[] args) {
 		
---
+++ GestioEmpleats/src/com/gestioempleats/components/FirstFrame.java
@@ -0,0 +1,78 @@
+package com.gestioempleats.components;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import com.jgoodies.forms.factories.FormFactory;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.RowSpec;
+
+public class FirstFrame extends JPanel {
+	private JTextField textFieldUser;
+	private JPasswordField passwordFieldPassword;
+	private JPasswordField passwordFieldValidatePassword;
+
+	public FirstFrame() {
+		setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("max(73dlu;default)"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("max(70dlu;default):grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("max(69dlu;default)"), }, new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, }));
+
+		JLabel lblFirstFrame = new JLabel("Crea un compte d'administrador.");
+		add(lblFirstFrame, "2, 2");
+
+		JLabel lblUser = new JLabel("Usuari:");
+		add(lblUser, "2, 4, right, default");
+
+		textFieldUser = new JTextField();
+		add(textFieldUser, "4, 4, fill, default");
+		textFieldUser.setColumns(10);
+
+		JLabel lblPassword = new JLabel("Contrassenya:");
+		add(lblPassword, "2, 6, right, default");
+
+		passwordFieldPassword = new JPasswordField();
+		add(passwordFieldPassword, "4, 6, fill, default");
+
+		JLabel lblValidatePassword = new JLabel("Confirma la contrassenya:");
+		add(lblValidatePassword, "2, 8, right, default");
+
+		passwordFieldValidatePassword = new JPasswordField();
+		add(passwordFieldValidatePassword, "4, 8, fill, default");
+
+		JButton btnCreate = new JButton("Crear");
+		btnCreate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String[] data = new String[2];
+				if (passwordFieldPassword.equals(passwordFieldValidatePassword)) {
+					data[0] = textFieldUser.getText().toString();
+					data[1] = passwordFieldPassword.getText().toString();
+					this.getDades(data);
+				}
+
+			}
+
+			private String[] getDades(String[] dades) {
+				return dades;
+			}
+		});
+		add(btnCreate, "4, 10");
+	}
+
+
+}
