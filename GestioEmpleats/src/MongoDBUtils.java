1634d4614c6a56b5a6c2ddf4d24249f3b4349ccd
Degor <Degor@192.168.0.195>
2013-11-20 20:23:46 +0100

    Added zip4j, download and unzip of the mongoDB

--- GestioEmpleats/src/MongoDBUtils.java
+++ GestioEmpleats/src/MongoDBUtils.java
@@ -1,7 +1,9 @@
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.Properties;
+
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
 
 import org.apache.commons.io.FileUtils;
 
@@ -21,27 +23,30 @@ public class MongoDBUtils {
 	
 	public static void installMongoDExe() {
 		System.out.println("Starting install of mongoDB on " + MainFrame.path.getPathToMongoDB());
-		File makeDirMongoDB = new File(MainFrame.path.getPathToMongoDB());
-		makeDirMongoDB.mkdir();
-		// copy to :systemdrive:\mongodb files from resource mongodb
-		
+		ZipFile zipFile;
+		try {
+			zipFile = new ZipFile(MainFrame.path.getPathToMongoDB()+File.separator+"mongoDB.zip");
+			zipFile.extractAll(MainFrame.path.getPathToMongoDB());
+		} catch (ZipException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static void startDownloadMongoDB() {
 		System.out.println("Starting download of mongoDB...");
 		System.setProperty("http.agent", "Mozilla/5.0 (X11; Linux i686; rv:21.0) Gecko/20100101 Firefox/21.0");
+		
 		try {
 			if (System.getProperty("os.arch").contains("86") || System.getProperty("os.arch").contains("i386")) {
-				FileUtils.copyURLToFile(new URL("http://fastdl.mongodb.org/win32/mongodb-win-i386-2.4.8.zip"), new File(MainFrame.path.getPathToMongoDB()));
+				FileUtils.copyURLToFile(new URL("http://fastdl.mongodb.org/win32/mongodb-win32-i386-2.4.8.zip"), new File(MainFrame.path.getPathToMongoDB()+File.separator+"mongoDB.zip"));
 			} else if (System.getProperty("os.arch").contains("64")) {
-				FileUtils.copyURLToFile(new URL("http://fastdl.mongodb.org/win32/mongodb-win-i386-2.4.8.zip"), new File(MainFrame.path.getPathToMongoDB()));
+				FileUtils.copyURLToFile(new URL("http://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2008plus-2.4.8.zip"), new File(MainFrame.path.getPathToMongoDB()+File.separator+"mongoDB.zip"));
 			} else {
 				System.out.println("Unknow architecture.");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
 	
 	public static void saveDataToDB() {
