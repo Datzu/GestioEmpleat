f9f2f88dc39e3b3c9c780b9b30b3dad88da092a8
Datzu <Degor@Degor-Portatil>
2013-11-26 09:24:57 +0100

    Added List to programmer

--- GestioEmpleats/src/com/gestioempleats/employeetypes/Programmer.java
+++ GestioEmpleats/src/com/gestioempleats/employeetypes/Programmer.java
@@ -1,15 +1,20 @@
 package com.gestioempleats.employeetypes;
 
+import java.util.List;
+
 public class Programmer extends Employee {
 	
-	private String languages;
+	private List<String> languages;
 
 	public Programmer(int employeeId, String user, String password,
 			String name, String lastname1, String lastname2, String birthday,
-			String contractDate, float income, int level, String languages) {
+			String contractDate, float income, int level) {
 		super(employeeId, user, password, name, lastname1, lastname2, birthday,
 				contractDate, income, level);
-		this.languages = languages;
+	}
+	
+	public void addLanguage(String language) {
+		this.languages.add(language);
 	}
 
 }
